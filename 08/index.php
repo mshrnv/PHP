00f698758bc42c292556ce7ae848c851dd0ebfd8
2020-5-24-sho <cool.shoronov19@gmail.com>
2021-12-01 11:59:57 +0300

    08: Реализована логика календаря

--- 08/index.php
+++ 08/index.php
@@ -1,14 +1,15 @@
 <?php
-    // Обработка GET
-    if (isset($_GET['year']) && isset($_GET['month'])) {
-        print getCalendar($_GET['month'], $_GET['year']);
-    }
-    else {
-        $html  = getHeader();
-        $html .= getCalendar(date('n'), date('Y'));
-        $html .= getFooter();
-        print $html;
-    }
+
+// Обработка GET
+if (isset($_GET['year']) && isset($_GET['month'])) {
+    print getCalendar($_GET['month'], $_GET['year']);
+}
+else {
+    $html  = getHeader();
+    $html .= getCalendar(date('n'), date('Y'));
+    $html .= getFooter();
+    print $html;
+}
 
 function getCalendar($__month, $__year)
 {
@@ -17,12 +18,7 @@ function getCalendar($__month, $__year)
 
     $dates = range(1, $daysCount);
 
-    $firstDayOfWeek = date('w', $timestamp);
-    foreach (range(1, $firstDayOfWeek) as $value) {
-        
-    }
-
-    return '<table class="table table-bordered">
+    $html = '<table class="table table-bordered">
             <tr>
                 <td>Monday</td>
                 <td>Tuesday</td>
@@ -31,126 +27,103 @@ function getCalendar($__month, $__year)
                 <td>Friday</td>
                 <td>Saturday</td>
                 <td>Sunday</td>
-            </tr>
-            <tr>
-                <td>1</td>
-                <td>2</td>
-                <td>3</td>
-                <td>4</td>
-                <td>5</td>
-                <td>6</td>
-                <td>7</td>
-            </tr>
-            <tr>
-                <td>8</td>
-                <td>9</td>
-                <td>10</td>
-                <td>11</td>
-                <td>12</td>
-                <td>13</td>
-                <td>14</td>
-            </tr>
-            <tr>
-                <td>15</td>
-                <td>16</td>
-                <td>17</td>
-                <td>18</td>
-                <td>19</td>
-                <td>20</td>
-                <td>21</td>
-            </tr>
-            <tr>
-                <td>22</td>
-                <td>23</td>
-                <td>24</td>
-                <td>25</td>
-                <td>26</td>
-                <td>27</td>
-                <td>28</td>
-            </tr>
-            <tr>
-                <td>29</td>
-                <td>30</td>
-                <td>31</td>
-                <td></td>
-                <td></td>
-                <td></td>
-                <td></td>
-            </tr>
-            <tr></tr></table>';
+            </tr>';
+    
+    $firstDayOfWeek = date('w', $timestamp);
+    $beginEmptyDaysCount = $firstDayOfWeek == 0 ? 6 : $firstDayOfWeek - 1;
+    for($day = 0; $day < $beginEmptyDaysCount; $day++) {
+        array_unshift($dates, '');
     }
 
-    function getHeader()
-    {
-        return '<!DOCTYPE html>
-                <html lang="en">
-                <head>
-                    <meta charset="UTF-8">
-                    <title>Calendar</title>
-                    <link rel="stylesheet" href="css/bootstrap.css">
-                    <link rel="stylesheet" href="css/style.css">
-                    <script src="js/jquery-3.6.0.min.js"></script>
-                    <script src="js/script.js"></script>
-                </head>
-                <body>
-                    <div class="container-fluid">
-                        <div class="row">
-                            <nav class="navbar navbar-light bg-light">
-                                <div class="container-fluid">
-                                    <a class="navbar-brand" href="#">
-                                        <img src="img/calendar.png" alt="Icon" width="30" height="30" class="d-inline-block align-text-top">
-                                        Calendar
-                                    </a>
-                                </div>
-                            </nav>
-                        </div>
-                        <div class="row" style="margin-top: 15px;">
-                            <div class="col-md-3"></div>
-                            <div class="col-md-3">
-                                <select class="form-select" id="year" onchange="update()">
-                                    <option value="2016">2016</option>
-                                    <option value="2017">2017</option>
-                                    <option value="2018">2018</option>
-                                    <option value="2019">2019</option>
-                                    <option value="2020">2020</option>
-                                    <option value="2021" selected>2021</option>
-                                    <option value="2022">2022</option>
-                                    <option value="2023">2023</option>
-                                    <option value="2024">2024</option>
-                                </select>
-                            </div>
-                            <div class="col-md-3">
-                                <select class="form-select" id="month" onchange="update();">
-                                    <option value="01">January</option>
-                                    <option value="02">February</option>
-                                    <option value="03">March</option>
-                                    <option value="04">April</option>
-                                    <option value="05">May</option>
-                                    <option value="06">June</option>
-                                    <option value="07">Jule</option>
-                                    <option value="08">August</option>
-                                    <option value="09">September</option>
-                                    <option value="10">Octomber</option>
-                                    <option value="11">November</option>
-                                    <option value="12" selected>December</option>
-                                </select>
-                            </div>
-                            <div class="col-md-3"></div>
-                        </div>
-                        <div class="row" style="margin-top: 25px;">
-                            <div class="col-md-2">
-
-                            </div>
-                            <div class="col-md-8" id="calendar">';
+    $endEmptyDaysCount = 7 - (count($dates) % 7);
+    for ($day = 0; $day < $endEmptyDaysCount; $day++) {
+        array_push($dates, '');
     }
 
-    function getFooter()
-    {
-        return '</div>
-                <div class="col-md-2"></div>
-            </div>
+    foreach(range(1, count($dates) / 7) as $week) {
+        $html .= '<tr>';
+        foreach (range(1,7) as $day) {
+            $html .= '<td>'.array_shift($dates).'</td>';
+        }
+        $html .= '</tr>';
+    }
+
+    $html .= '</table>';
+
+    return $html;
+}
+
+function getHeader()
+{
+    return '<!DOCTYPE html>
+            <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <title>Calendar</title>
+                <link rel="stylesheet" href="css/bootstrap.css">
+                <link rel="stylesheet" href="css/style.css">
+                <script src="js/jquery-3.6.0.min.js"></script>
+                <script src="js/script.js"></script>
+            </head>
+            <body>
+                <div class="container-fluid">
+                    <div class="row">
+                        <nav class="navbar navbar-light bg-light">
+                            <div class="container-fluid">
+                                <a class="navbar-brand" href="#">
+                                    <img src="img/calendar.png" alt="Icon" width="30" height="30" class="d-inline-block align-text-top">
+                                    Calendar
+                                </a>
+                            </div>
+                        </nav>
+                    </div>
+                    <div class="row" style="margin-top: 15px;">
+                        <div class="col-md-3"></div>
+                        <div class="col-md-3">
+                            <select class="form-select" id="year" onchange="update()">
+                                <option value="2016">2016</option>
+                                <option value="2017">2017</option>
+                                <option value="2018">2018</option>
+                                <option value="2019">2019</option>
+                                <option value="2020">2020</option>
+                                <option value="2021" selected>2021</option>
+                                <option value="2022">2022</option>
+                                <option value="2023">2023</option>
+                                <option value="2024">2024</option>
+                            </select>
+                        </div>
+                        <div class="col-md-3">
+                            <select class="form-select" id="month" onchange="update();">
+                                <option value="01">January</option>
+                                <option value="02">February</option>
+                                <option value="03">March</option>
+                                <option value="04">April</option>
+                                <option value="05">May</option>
+                                <option value="06">June</option>
+                                <option value="07">Jule</option>
+                                <option value="08">August</option>
+                                <option value="09">September</option>
+                                <option value="10">Octomber</option>
+                                <option value="11">November</option>
+                                <option value="12" selected>December</option>
+                            </select>
+                        </div>
+                        <div class="col-md-3"></div>
+                    </div>
+                    <div class="row" style="margin-top: 25px;">
+                        <div class="col-md-2">
+
+                        </div>
+                        <div class="col-md-8" id="calendar">';
+}
+
+function getFooter()
+{
+    return '</div>
+            <div class="col-md-2"></div>
         </div>
-    </body>
-    </html>';
-    }
+    </div>
+</body>
+</html>';
+}
                 
